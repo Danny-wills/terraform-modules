e8b9ff392cd18eaa795804d83a34be0c8b502e31
Ojo-williams Daniel <ojowilliamsdaniel@gmail.com>
2023-06-06 04:08:53 +0100

    route53

---
+++ Route53/main.tf
@@ -0,0 +1,18 @@
+# get hosted zone details
+data "aws_route53_zone" "hosted_zone" {
+  name = var.hosted_zone
+}
+
+# create a record set in route 53
+resource "aws_route53_record" "site_domain" {
+  zone_id = data.aws_route53_zone.hosted_zone.id
+  name    = var.record_name
+  type    = "A"
+
+  alias {
+    name                   = var.alb_dns_name
+    zone_id                = var.alb_zone_id
+    evaluate_target_health = true
+  }
+}
+
---
+++ Route53/variable.tf
@@ -0,0 +1,4 @@
+variable "hosted_zone" {}
+variable "record_name"  {}
+variable "alb_dns_name" {}
+variable "alb_zone_id" {}
