0c0856341ef3007049c386b8da9cf7db479457e7
Ojo-williams Daniel <ojowilliamsdaniel@gmail.com>
2023-06-06 02:31:19 +0100

    changes

--- acm/main.tf
+++ acm/main.tf
@@ -1,6 +1,6 @@
 # get certificate from acm
 data "aws_acm_certificate" "amazon_issued" {
-  domain      = var.domain
+  domain      = var.domain_name
   types       = ["AMAZON_ISSUED"]
   most_recent = true
 }
